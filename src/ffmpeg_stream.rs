ae571582e9a3ad707ead378c3a791fa024b878d9
arch <arch@local>
2023-01-28 21:42:11 +0100

    add untested transform func

--- src/ffmpeg_stream.rs
+++ src/ffmpeg_stream.rs
@@ -14,6 +14,7 @@ use std::io::{BufRead, BufReader};
 use std::marker::PhantomData;
 use std::process::Stdio;
 use std::sync::Arc;
+use tokio::io::AsyncWriteExt;
 use tokio::process::Command;
 use tokio_util::codec::Decoder;
 use tokio_util::codec::FramedRead;
@@ -138,13 +139,57 @@ pub fn get_video_fps(video_path: &str) -> Result<f32, Box<dyn std::error::Error>
     }
 }
 
+pub fn get_video_dimensions(video_path: &str) -> Result<Dimensions, Box<dyn std::error::Error>> {
+    let mut cmd = std::process::Command::new("ffprobe")
+        .args([
+            "-v",
+            "error",
+            "-select_streams",
+            "v:0",
+            "-show_entries",
+            "stream=width,height",
+            "-of",
+            "csv=s=x:p=0",
+            video_path,
+        ])
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let stdout = cmd.stdout.as_mut().unwrap();
+    let stdout_reader = BufReader::new(stdout);
+    let resolution = stdout_reader.lines().next().unwrap()?;
+
+    let mut video_dimensions: Dimensions = Dimensions {
+        width: 0,
+        height: 0,
+    };
+
+    let re = regex::Regex::new(r"(\d+)x(\d+)").unwrap();
+    for cap in re.captures_iter(&resolution) {
+        let w = cap[1].parse::<u32>()?;
+        let h = cap[2].parse::<u32>()?;
+        video_dimensions = Dimensions::new(w, h);
+    }
+
+    cmd.wait()?;
+
+    info!(
+        "Video resolution: {}x{}",
+        video_dimensions.width, video_dimensions.height
+    );
+
+    Ok(video_dimensions)
+}
+
 pub async fn spawn_ffmpeg_frame_reader(
     args: Vec<&str>,
     frame_dimensions: Dimensions,
+    input: Option<&[u8]>,
 ) -> FramedRead<tokio::process::ChildStdout, VideoFrame> {
     let mut cmd = Command::new("ffmpeg");
     cmd.args(args);
 
+    cmd.stdin(Stdio::piped());
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::null());
 
@@ -155,12 +200,25 @@ pub async fn spawn_ffmpeg_frame_reader(
         .take()
         .expect("ffmpeg process did not have a handle to stdout");
 
+    let mut stdin = child
+        .stdin
+        .take()
+        .expect("ffmpeg process did not have a handle to stdin");
+
     tokio::spawn(async move {
         let _ = child
             .wait()
             .await
             .expect("ffmpeg process encountered an error");
     });
+
+    if let Some(input) = input {
+        if stdin.write_all(&input).await.is_err() {
+            error!("ffmpeg stdin failed");
+        }
+    }
+
+    drop(stdin);
 
     FramedRead::new(
         stdout,
@@ -172,10 +230,8 @@ pub async fn get_single_frame(
     video_path: &str,
     timestamp_in_ms: u32,
 ) -> Result<Option<FFmpegFrame>, Box<dyn std::error::Error>> {
-    // TODO determine real video resolution
-    let d = Dimensions {
-        width: 4096,
-        height: 2048,
+    let Ok(video_dimensions) = get_video_dimensions(video_path) else {
+        return Ok(None);
     };
 
     let mut reader = spawn_ffmpeg_frame_reader(
@@ -203,14 +259,80 @@ pub async fn get_single_frame(
             "-sn",
             "-",
         ],
-        d,
+        video_dimensions,
+        None,
     )
     .await;
 
     if let Some(Ok(bytes_mut_buffer)) = reader.next().await {
-        let frame_buffer: FrameBuffer =
-            FrameBuffer::from_raw(d.width, d.height, bytes_mut_buffer.to_vec())
-                .expect("ffmpeg: parse frame error");
+        let frame_buffer: FrameBuffer = FrameBuffer::from_raw(
+            video_dimensions.width,
+            video_dimensions.height,
+            bytes_mut_buffer.to_vec(),
+        )
+        .expect("ffmpeg: parse frame error");
+        Ok(Some(FFmpegFrame::new(frame_buffer)))
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn transform_frame(
+    frame: DynamicImage,
+    video_filter: &str,
+) -> Result<Option<FFmpegFrame>, Box<dyn std::error::Error>> {
+    let input_dimensions = Dimensions {
+        width: frame.width(),
+        height: frame.height() as u32,
+    };
+    // TODO ensure input image is min 1,5x larger than output (use image resize)
+    let Some(output_dimensions) = get_dimension_from_video_filter(video_filter) else {
+        error!("Failed to parse video filter dimensions");
+        return Ok(None);
+    };
+
+    let mut reader = spawn_ffmpeg_frame_reader(
+        vec![
+            "-hide_banner",
+            "-loglevel",
+            "error",
+            "-y",
+            "-f",
+            "rawvideo",
+            "-vcodec",
+            "rawvideo",
+            "-s",
+            format!("{}x{}", input_dimensions.width, input_dimensions.height).as_str(),
+            "-pix_fmt",
+            "bgr24",
+            "-i",
+            "-",
+            "-f",
+            "image2pipe",
+            "-pix_fmt",
+            "bgr24",
+            "-vsync",
+            "passthrough",
+            "-vcodec",
+            "rawvideo",
+            "-an",
+            "-sn",
+            "-vf",
+            video_filter,
+            "-",
+        ],
+        output_dimensions,
+        Some(&frame.into_rgb8()),
+    )
+    .await;
+
+    if let Some(Ok(bytes_mut_buffer)) = reader.next().await {
+        let frame_buffer: FrameBuffer = FrameBuffer::from_raw(
+            output_dimensions.width,
+            output_dimensions.height,
+            bytes_mut_buffer.to_vec(),
+        )
+        .expect("ffmpeg: parse frame error");
         Ok(Some(FFmpegFrame::new(frame_buffer)))
     } else {
         Ok(None)
@@ -300,6 +422,7 @@ pub async fn ffmpeg_stream_reader(
             "-",
         ],
         video_dimensions,
+        None,
     )
     .await;
 
