c5684c9699bce2a2575d2e5162a5cd4b79e788f7
arch <arch@local>
2023-01-31 10:49:56 +0100

    cross compiling tests (not working)

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+      in rec {
+        defaultPackage = packages.x86_64-pc-windows-gnu;
+
+        packages.x86_64-pc-windows-gnu = naersk'.buildPackage {
+          src = ./.;
+          strictDeps = true;
+
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            # We need Wine to run tests:
+            wineWowPackages.stable
+          ];
+
+          doCheck = true;
+
+preBuild = ''
+  export PKG_CONFIG_PATH=/mnt/Data_003/Work/Repos/mirror/mtfg-rs/mingw64/lib/pkgconfig
+  export OPENCV_LINK_LIBS="opencv_imgproc,opencv_core"
+  export OPENCV_LINK_PATHS=/mnt/Data_003/Work/Repos/mirror/mtfg-rs/mingw64/lib
+  export OPENCV_INCLUDE_PATHS=/mnt/Data_003/Work/Repos/mirror/mtfg-rs/mingw64/include/opencv4
+  export OpenCV_DIR=/mnt/Data_003/Work/Repos/mirror/mtfg-rs/mingw64/lib/cmake/opencv4
+  export PATH=/mnt/Data_003/Work/Repos/mirror/mtfg-rs/mingw64/bin:$PATH
+  export RUST_BACKTRACE=1
+'';
+
+
+          # Tells Cargo that we're building for Windows.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#buildtarget)
+          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+
+          # Tells Cargo that it should use Wine to run tests.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerunner)
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+            export WINEPREFIX="$(mktemp -d)"
+            exec wine64 $@
+          '';
+        };
+      }
+    );
+}
