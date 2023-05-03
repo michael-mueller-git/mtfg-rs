5f0fc1f8ae7a75e3777e0a00cbc01e708b0511ff
arch <arch@local>
2023-05-03 19:44:30 +0200

    flake testing

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   outputs = { self, nixpkgs, naersk }:
     let
       cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
@@ -51,7 +51,7 @@
         {
           format = pkgs.runCommand "check-format"
             {
-              buildInputs = with pkgs; [ rustfmt cargo ];
+              buildInputs = with pkgs; [ rustfmt cargo opencv ];
             } ''
             ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
             ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
@@ -73,6 +73,7 @@
           buildInputs = with pkgs; [
             rustfmt
             nixpkgs-fmt
+            opencv
           ];
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         });
