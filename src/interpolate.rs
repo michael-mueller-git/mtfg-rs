86cb5dc9bb4c406e09eac394b714dda55d035cd6
arch <arch@local>
2023-05-04 19:20:17 +0200

    code fmt

--- src/interpolate.rs
+++ src/interpolate.rs
@@ -9,7 +9,7 @@ pub fn interpolate_score(
     }
 
     if frame_step_size <= 1 {
-        return Some(raw_score)
+        return Some(raw_score);
     }
 
     let x = raw_score
@@ -23,7 +23,12 @@ pub fn interpolate_score(
 
     let mut spline = mentat::MonotonicCubicSpline::new(&x, &y);
 
-    Some((raw_score.first().unwrap().x .. raw_score.first().unwrap().x + raw_score.len() as i32)
-        .map(|x| mint::Point2 {x: x, y: spline.interpolate(x.into()) as i32})
-        .collect::<Vec<_>>())
+    Some(
+        (raw_score.first().unwrap().x..raw_score.first().unwrap().x + raw_score.len() as i32)
+            .map(|x| mint::Point2 {
+                x: x,
+                y: spline.interpolate(x.into()) as i32,
+            })
+            .collect::<Vec<_>>(),
+    )
 }
