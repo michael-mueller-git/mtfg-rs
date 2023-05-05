09852f0b7c4772b398f46f83fb079eef879ff3ff
arch <arch@local>
2023-05-05 21:06:32 +0200

    flake testing

--- flake.nix
+++ flake.nix
@@ -4,17 +4,29 @@
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+      nixpkgs.overlays = [
+        (self: super: {
+          yabai = super.opencv.overrideAttrs (old: rec {
+            buildInputs = old.buildInputs ++ [pkgs.qt5.full];
+            cmakeFlags = old.cmakeFlags ++ ["-DWITH_QT=ON"];
+          });
+        })
+      ];
   };
 
   outputs = { self, nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+
+
         pkgs = import nixpkgs {
           inherit system;
         };
         craneLib = crane.lib.${system};
       in
     {
+
+
       packages.default = craneLib.buildPackage {
         src = craneLib.cleanCargoSource (craneLib.path ./.);
         buildInputs = [ pkgs.opencv ];
